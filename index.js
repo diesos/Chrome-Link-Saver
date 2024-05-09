186404536602e9cb62469851b117798feda3dfdc
Diesos <ozturk@live.Fr>
2024-05-09 22:23:29 +0200

    chore: Refactor code to use localStorage for storing leads

--- index.js
+++ index.js
@@ -2,23 +2,37 @@ let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+let leadsLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
-inputBtn.addEventListener("click", function() {
+if (leadsLocalStorage) {
+    myLeads = leadsLocalStorage
+    render(myLeads)
+}
+
+inputBtn.addEventListener("click", function () {
     myLeads.push(inputEl.value)
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
     inputEl.value = ""
-    renderLeads()
+    render(myLeads)
 })
 
-function renderLeads() {
+deleteBtn.addEventListener('dblclick', () => {
+    localStorage.clear();
+    myLeads = []
+    render(myLeads)
+})
+
+function render(leads) {
     let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) {
+    for (let i = 0; i < leads.length; i++) {
         listItems += `
             <li>
-                <a target='_blank' href='${myLeads[i]}'>
-                    ${myLeads[i]}
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
                 </a>
             </li>
         `
     }
-    ulEl.innerHTML = listItems  
+    ulEl.innerHTML = listItems
 }
