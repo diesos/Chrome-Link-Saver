f3e4bc8656c37becc54f6b18c4f5109ca5033c70
Diesos <ozturk@live.Fr>
2024-05-12 21:36:57 +0200

    Added new function : Save actual tab

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 const deleteBtn = document.getElementById("delete-btn")
+const tabBTn = document.getElementById("savetab-btn")
 let leadsLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
 if (leadsLocalStorage) {
@@ -15,6 +16,14 @@ inputBtn.addEventListener("click", function () {
     localStorage.setItem("myLeads", JSON.stringify(myLeads))
     inputEl.value = ""
     render(myLeads)
+})
+
+tabBTn.addEventListener('click' , () => {
+    let getUrl = window.location.href
+    myLeads.push(getUrl)
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads)
+
 })
 
 deleteBtn.addEventListener('dblclick', () => {
